6b5397a469cab052f3e7105d5030abb1eefa8daa
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 16:44:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def load_library(file)
 end
 
 def get_japanese_emoticon(file, emoticon)
-  # code goes here
+  #code goes here
   library = load_library(file)
   word = library.keys.find do |key|
     library[key][:english] == emoticon  
