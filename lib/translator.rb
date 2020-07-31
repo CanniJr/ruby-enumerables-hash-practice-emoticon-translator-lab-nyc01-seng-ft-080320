c2504ca4774ab6ba679a09c2a996003cc4322b50
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 16:34:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,18 +5,7 @@ require 'pry'
 
 def load_library
   # code goes here
-  require 'yaml'
-  # code goes here	require 'pry'
-
-def load_library(file)
-  emoticons = YAML.load_file(file)
-  new_hash = {}
-  emoticons.each do |key, value|
-    new_hash[key] = {}
-    new_hash[key][:english] = value[0]
-    new_hash[key][:japanese] = value[1]
-    end
-  new_hash 
+  
 end
 
 def get_japanese_emoticon
